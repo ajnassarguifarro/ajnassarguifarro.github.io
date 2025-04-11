84b109875ffafd99d55696fee4ec302170e8defb
ajnassarguifarro <ajnassarguifarro@my.waketech.edu>
2025-04-11 16:32:25 -0400

    resubmitted gradebook.js and html

---
+++ Gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+    console.log("Fetching Grade Data...");
+}
+
+function populateGradebook(data){
+    console.log("Populating Gradbook With Data:", data)
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
