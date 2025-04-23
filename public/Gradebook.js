994522537b0bf60fbda8bb59f27a7694f00c87b8
ajnassarguifarro <ajnassarguifarro@my.waketech.edu>
2025-04-23 17:49:18 -0400

    updated gradebook.js file

--- public/Gradebook.js
+++ public/Gradebook.js
@@ -1,9 +1,34 @@
 function fetchGradeData() {
     console.log("Fetching Grade Data...");
+    let xhr = new XHLHttpRequest();
+    let apiRout = "/api/grades";
+    xhr.onreadystatechange = function(){
+        let rusults;
+        if(xhr.readyState === xhr.DONE){
+            if (xhr.status !== 200){
+                console.error('Could not get grades.');
+                    Status: {xhr.status}
+            }
+            populationGradebook(JSON.parse(xhr.responseText));
+            }
+    }.bind(this);
+    xhr.open("get", apiRoute, true);
+    xhr.send();
 }
 
 function populateGradebook(data){
-    console.log("Populating Gradbook With Data:", data)
+    console.log("Populating Gradbook With Data:", data);
+    let tableElm = document.getElementById("gradebook");
+        data.forEach(function(assignment){
+            let row = document.createElement("tr");
+            let columns = [];
+            columns.name = document.createElement('td');
+            columns.name.appendChild(document.createTextNode(assignment.last_name + " , " + assignment.first_name)
+    );
+    row.appendChild(columns.name);
+    row.appendChild(columns.grade);
+    tableElm.appendChild(row); 
+    });
 }
 
 const gradeData = fetchGradeData();
